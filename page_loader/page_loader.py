3a745d9c599761e66b098b52c9cc22219582e047
Marfa Nikitina <marfik93@mail.ru>
2022-08-07 17:54:13 +0300

    07/08

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,19 +1,50 @@
 import requests
-# from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 import os
 import re
 
 
-def download(filepath, url, output=os.getcwd()):
+def to_file(url):
     url_without_extension = os.path.splitext(url)[0]
     file_name_list = re.split('\//|\/|\.', url_without_extension)[1:]
     file_name = '-'.join(file_name_list) + '.html'
-    new_fp = os.path.join(filepath, file_name)
+    return file_name
+
+
+def to_dir(url):
+    file_name = to_file(url)
+    dir_name = os.path.splitext(file_name)[0] + '_files'
+    return dir_name
+
+
+
+def download(filepath, url, output=os.getcwd()):
+    new_fp = os.path.join(filepath, to_file(url))
     send_request = requests.get(url)
     with open(new_fp, 'w') as file:
         file.write(send_request.text)
+    dir_name = os.path.join(filepath, to_dir(url))
     return new_fp
 
+
+# def read(file_path):
+#     with open(file_path, 'r') as f:
+#         result = f.read()
+#     return result
+# 
+# 
+# def write(file_path, data):
+#     with open(file_path, 'w') as f:
+#         f.write(data)
+# 
+# 
+# def _right(file_path):
+#     data = read(file_path)
+# 
+#     write(
+#         file_path,
+#         BeautifulSoup(data, 'html.parser').prettify()
+#     )
 
 # with open("index.html") as fp:
     # soup = BeautifulSoup(fp, 'html.parser')
--- tests/test_download.py
+++ tests/test_download.py
@@ -1,21 +1,88 @@
 import pytest
 import os
-from page_loader.page_loader import download
-import requests
-# import requests_mock
+from page_loader.page_loader import download, to_file, to_dir
+import tempfile
+import requests_mock
 
 
 TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
 FIXTURES_PATH = f"{TESTS_DIR}/fixtures"
 
 
-@pytest.mark.parametrize('file_path, url, res_file_path',
-                         [('./tmp', 'https://ru.hexlet.io/courses', 'ru-hexlet-io-courses.txt')])
-def test_download(url, file_path, res_file_path):
-    fixtures_file = f"{FIXTURES_PATH}/{res_file_path}"
-    with open(fixtures_file, 'r') as data:
-        expected = data.read()
-    result_file = download(file_path, url,  output=os.getcwd())
-    with open(result_file, 'r') as data:
+URL = 'https://ru.hexlet.io/courses'
+
+EXPECTED_HTML = f"{FIXTURES_PATH}/ru-hexlet-io-courses.txt"
+
+DOWNLOADED_HTML = './tmp/ru-hexlet-io-courses.html'
+
+
+@pytest.mark.parametrize('url, expected_result',
+                         [('https://ru.hexlet.io/courses', 'ru-hexlet-io-courses.html')])
+def test_download(url, expected_result):
+    html_expected = read(EXPECTED_HTML)
+
+    with requests_mock.Mocker() as mock:
+        with tempfile.TemporaryDirectory() as tmpdir:
+            mock.get(URL, text=html_expected)
+            download(tmpdir, URL)
+            actual_html = read(os.path.join(tmpdir, DOWNLOADED_HTML))
+
+            assert actual_html == html_expected
+
+
+def read(file_path):
+    with open(file_path, 'r') as data:
         result = data.read()
-    assert result == expected
+    return result
+
+
+@pytest.mark.parametrize('url, expected_filename',
+                         [('https://ru.hexlet.io/courses', 'ru-hexlet-io-courses.html')])
+def test_to_file(url, expected_filename):
+    assert to_file(url) == expected_filename
+
+
+@pytest.mark.parametrize('url, expected_dir_name',
+                         [('https://ru.hexlet.io/courses', 'ru-hexlet-io-courses_files')])
+def test_to_dir(url, expected_dir_name):
+    assert to_dir(url) == expected_dir_name
+
+
+
+# @pytest.mark.parametrize('file_path, url, res_file_path',
+#                          [('./tmp', 'https://ru.hexlet.io/courses', 'ru-hexlet-io-courses.txt')])
+# def test_download(url, file_path, res_file_path):
+#     fixtures_file = f"{FIXTURES_PATH}/{res_file_path}"
+#     with open(fixtures_file, 'r') as data:
+#         expected = data.read()
+#     result_file = download(file_path, url,  output=os.getcwd())
+#     with open(result_file, 'r') as data:
+#         result = data.read()
+#     assert result == expected
+# 
+# 
+# 
+# def get_fixture_path(name):
+#     return os.path.join('fixtures', name)
+# 
+# 
+# file_name = 'before.html'
+# src = get_fixture_path(file_name)
+# 
+# 
+# @pytest.fixture(scope='module')
+# def expected():
+#     return read(get_fixture_path('after.html'))
+# 
+# 
+# @pytest.fixture
+# def dest_file(tmpdir):
+#     dest = tmpdir.join(file_name)
+#     shutil.copyfile(src, dest)
+#     return dest
+# 
+# 
+# def test_prettify(dest_file, expected):
+#     prettify_html_file(dest_file)
+#     actual = read(dest_file)
+    # assert actual == expected
