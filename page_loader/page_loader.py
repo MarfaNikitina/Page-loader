f8f8c20667d5706116b4ba42121668d97ac23c65
Marfa Nikitina <marfik93@mail.ru>
2022-08-13 15:19:22 +0300

    13/08/01

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -29,60 +29,28 @@ def download(url, filepath=os.getcwd()):
     return new_fp
 
 
-def download_img(url, dir_name, soup):
-    images = [img for img in soup.findAll('img')]
-    print(str(len(images)) + " images found.")
-    print('Downloading images to current working directory.')
-    image_links = [each.get('src') for each in images]
-    for each in image_links:
-        try:
-            print(each)
-            image_name = to_image_name(url, each)
-            filename = os.path.join(dir_name, image_name)
-            src = urljoin(url, each)
-            print('Getting: ' + filename)
-            response = requests.get(src, stream=True)
-            time.sleep(1)
-            # write(response.raw, filename, binary=True)
-            with open(filename, 'wb') as out_file:
-                shutil.copyfileobj(response.raw, out_file)
-        except:
-            print('  An error occured. Continuing.')
-    print('Done.')
-
-
 def download_resources(url, dir_name, soup):
     resources = []
     links = [link for link in soup.findAll('link')]
     link_links = [each.get('href') for each in links]
-    for each in link_links:
-        try:
-            print(each)
-            link_name = to_image_name(url, each)
-            filename = os.path.join(dir_name, link_name)
-            href = urljoin(url, each)
-            # print('Getting: ' + filename)
-            response = requests.get(href, stream=True)
-            time.sleep(1)
-            with open(filename, 'wb') as out_file:
-                shutil.copyfileobj(response.raw, out_file)
-        except:
-            print('  An error occured. Continuing.')
-    print('Done.')
-    # resources.extend(links)
+    download_links(url, link_links, dir_name)
     images = [img for img in soup.findAll('img')]
     resources.extend(images)
     scripts = [script for script in soup.findAll('script') if script.get('src') is not None]
     resources.extend(scripts)
     resources_links = [each.get('src') for each in resources]
-    for each in resources_links:
+    download_links(url, resources_links, dir_name)
+    print('Done.')
+
+
+def download_links(url, list_of_links, dir_name):
+    for each in list_of_links:
         try:
             print(each)
             link_name = to_image_name(url, each)
             filename = os.path.join(dir_name, link_name)
-            src = urljoin(url, each)
-            # print('Getting: ' + filename)
-            response = requests.get(src, stream=True)
+            attr = urljoin(url, each)
+            response = requests.get(attr, stream=True)
             time.sleep(1)
             with open(filename, 'wb') as out_file:
                 shutil.copyfileobj(response.raw, out_file)
@@ -98,3 +66,53 @@ def write(file_path, data, binary=False):
     else:
         with open(file_path, 'wb') as f:
             f.write(data)
+
+
+# def download_img(url, dir_name, soup):
+#     images = [img for img in soup.findAll('img')]
+#     print(str(len(images)) + " images found.")
+#     print('Downloading images to current working directory.')
+#     image_links = [each.get('src') for each in images]
+#     for each in image_links:
+#         try:
+#             print(each)
+#             image_name = to_image_name(url, each)
+#             filename = os.path.join(dir_name, image_name)
+#             src = urljoin(url, each)
+#             print('Getting: ' + filename)
+#             response = requests.get(src, stream=True)
+#             time.sleep(1)
+#             # write(response.raw, filename, binary=True)
+#             with open(filename, 'wb') as out_file:
+#                 shutil.copyfileobj(response.raw, out_file)
+#         except:
+#             print('  An error occured. Continuing.')
+#     print('Done.')
+
+    # for each in resources_links:
+    #     try:
+    #         print(each)
+    #         link_name = to_image_name(url, each)
+    #         filename = os.path.join(dir_name, link_name)
+    #         src = urljoin(url, each)
+    #         # print('Getting: ' + filename)
+    #         response = requests.get(src, stream=True)
+    #         time.sleep(1)
+    #         with open(filename, 'wb') as out_file:
+    #             shutil.copyfileobj(response.raw, out_file)
+    #     except:
+    #         print('  An error occured. Continuing.')
+
+    # for each in link_links:
+    #     try:
+    #         print(each)
+    #         link_name = to_image_name(url, each)
+    #         filename = os.path.join(dir_name, link_name)
+    #         href = urljoin(url, each)
+    #         response = requests.get(href, stream=True)
+    #         time.sleep(1)
+    #         with open(filename, 'wb') as out_file:
+    #             shutil.copyfileobj(response.raw, out_file)
+    #     except:
+    #         print('  An error occured. Continuing.')
+    # print('Done.')
