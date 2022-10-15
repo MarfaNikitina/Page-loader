7d5d53f9cdfe6a655482f9edb89931738eb0a8b9
Marfa Nikitina <marfik93@mail.ru>
2022-10-15 11:49:25 +0300

    15/10

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -2,7 +2,7 @@ import os
 import requests
 import shutil
 import logging.config
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 from page_loader.name import to_filename, to_dir, to_resource_name
 from page_loader.resources import get_resources
 from urllib.parse import urljoin, urlparse
@@ -18,13 +18,10 @@ def download(url, filepath=os.getcwd()):
     new_file_name = os.path.join(filepath, to_filename(url))
     dir_name = to_dir(url)
     dir_path = os.path.join(filepath, dir_name)
-    response = requests.get(url)
     create_directory(dir_path)
-
-    soup = BeautifulSoup(response.content, 'html.parser')
     resources, html = get_resources(url, dir_name)
     logger_info.info(f'Downloading resources from {url}')
-    download_resources(url, dir_path, soup)
+    download_resources(resources, url, dir_path)
     logger_info.info(f'Downloading html from {url}')
     save(new_file_name, html)
     return new_file_name
@@ -38,26 +35,11 @@ def create_directory(dir_path):
         logger_info.info(f'Directory {dir_path} has been already created.')
 
 
-def download_resources(url, dir_name, soup):
-    resources = []
-    link_links = get_tags_links('link', 'href', soup)
-    resources.extend(link_links)
-    images = get_tags_links('img', 'src', soup)
-    resources.extend(images)
-    scripts = get_tags_links('script', 'src', soup)
-    resources.extend(scripts)
+def download_resources(resources, url, dir_name):
     for resource in resources:
         link = urlparse(resource)
         if link.netloc == urlparse(url).netloc or link.netloc == '':
             download_links(url, resource, dir_name)
-
-
-def get_tags_links(tag, attribute, soup):
-    tags = [tag for tag in soup.findAll(tag)]
-    tags_links = [
-        each.get(attribute) for each in tags if each.get(attribute) is not None
-    ]
-    return tags_links
 
 
 def download_links(url, link, dir_name):
--- page_loader/resources.py
+++ page_loader/resources.py
@@ -2,26 +2,22 @@ from bs4 import BeautifulSoup
 import requests
 import os
 from page_loader.name import to_resource_name
+# from urllib.parse import urljoin, urlparse
 
 
 def get_resources(url, dir_name):
     response = requests.get(url)
     data = BeautifulSoup(response.content, 'html.parser')
-    # tegs = ['img', 'link', 'script']
-    resources = []
-    images = [img for img in data.findAll('img')]
-    links = [link for link in data.findAll('link')]
-    scripts = [
-        s for s in data.findAll('script') if s.get('src') is not None
-    ]
-    resources.extend(images)
-    resources.extend(links)
-    resources.extend(scripts)
+    tags = ['img', 'link', 'script']
+    resources = [tag for tag in data.findAll(tags)]
+    tags_links = []
     for each in resources:
         if each.get('href') is not None:
+            tags_links.append(each.get('href'))
             resource_name = to_resource_name(url, each['href'])
             each['href'] = os.path.join(dir_name, resource_name)
-        else:
+        elif each.get('src') is not None:
+            tags_links.append(each.get('src'))
             resource_name = to_resource_name(url, each['src'])
             each['src'] = os.path.join(dir_name, resource_name)
-    return resources, data.prettify()
+    return tags_links, data.prettify()
