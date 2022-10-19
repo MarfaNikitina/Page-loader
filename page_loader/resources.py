71a04a05c13547624b5d995dcf221c6d17fe06f9
Marfa Nikitina <marfik93@mail.ru>
2022-10-19 14:55:04 +0300

    19/10/02

--- page_loader/resources.py
+++ page_loader/resources.py
@@ -5,7 +5,7 @@ import os
 from page_loader.log import LOGGING_CONFIG
 from page_loader.name import to_resource_name
 from page_loader.log import logger_error, logger_info
-# from urllib.parse import urljoin, urlparse
+from urllib.parse import urljoin, urlparse
 
 
 logging.config.dictConfig(LOGGING_CONFIG)
@@ -32,12 +32,14 @@ def get_resources(response, url, dir_name):
     resources = [tag for tag in data.findAll(tags)]
     tags_links = []
     for each in resources:
-        if each.get('href') is not None:
-            tags_links.append(each.get('href'))
-            resource_name = to_resource_name(url, each['href'])
-            each['href'] = os.path.join(dir_name, resource_name)
-        elif each.get('src') is not None:
-            tags_links.append(each.get('src'))
-            resource_name = to_resource_name(url, each['src'])
-            each['src'] = os.path.join(dir_name, resource_name)
+        link = urlparse(each)
+        if link.netloc == urlparse(url).netloc or link.netloc == '':
+            if each.get('href') is not None:
+                tags_links.append(each.get('href'))
+                resource_name = to_resource_name(url, each['href'])
+                each['href'] = os.path.join(dir_name, resource_name)
+            elif each.get('src') is not None:
+                tags_links.append(each.get('src'))
+                resource_name = to_resource_name(url, each['src'])
+                each['src'] = os.path.join(dir_name, resource_name)
     return tags_links, data.prettify()
